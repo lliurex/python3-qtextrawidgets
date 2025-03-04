30275530bc74090cafed702b06cc18db9e861f71
lliurex <juanma1980@gmail.com>
2025-03-04 13:05:16 +0100

    implement clear layout

--- QtExtraWidgets/QFlowTouchWidget.py
+++ QtExtraWidgets/QFlowTouchWidget.py
@@ -7,66 +7,74 @@ import PySide2
 class _layout(QLayout):
 	def __init__(self, parent=None):
 		super().__init__(parent)
-		self._item_list = []
+		self._itemList = []
 		if parent is not None:
 			self.setContentsMargins(0, 0, 0, 0)
+	#def __init__
 
 	def __del__(self):
 		item = self.takeAt(0)
 		while item:
 			item = self.takeAt(0)
+	#def __del__
 
 	def addItem(self, item):
-		self._item_list.append(item)
+		self._itemList.append(item)
+	#def addItem
 
 	def count(self):
-		return len(self._item_list)
+		return len(self._itemList)
+	#def count
 
 	def itemAt(self, index):
-		if 0 <= index < len(self._item_list):
-			return self._item_list[index]
-
+		if 0 <= index < len(self._itemList):
+			return self._itemList[index]
 		return None
+	#def itemAt
 
 	def takeAt(self, index):
-		if 0 <= index < len(self._item_list):
-			return self._item_list.pop(index)
-
+		if 0 <= index < len(self._itemList):
+			return self._itemList.pop(index)
 		return None
+	#def takeAt
 
 	def expandingDirections(self):
 		return Qt.Orientation(0)
+	#def expandingDirections
 
 	def hasHeightForWidth(self):
 		return True
+	#def hasHeightForWidth
 
 	def heightForWidth(self, width):
-		height = self._do_layout(QRect(0, 0, width, 0), True)
+		height = self.doLayout(QRect(0, 0, width, 0), True)
 		return height
+	#def heightForWidth
 
 	def setGeometry(self, rect):
 		super(_layout, self).setGeometry(rect)
-		self._do_layout(rect, False)
+		self.doLayout(rect, False)
+	#def setGeometry
 
 	def sizeHint(self):
 		return self.minimumSize()
+	#def sizeHint
 
 	def minimumSize(self):
 		size = QSize()
-
-		for item in self._item_list:
+		for item in self._itemList:
 			size = size.expandedTo(item.minimumSize())
-
 		size += QSize(2 * self.contentsMargins().top(), 2 * self.contentsMargins().top())
 		return size
+	#def minimumSize
 
-	def _do_layout(self, rect, test_only):
+	def doLayout(self, rect, test_only):
 		x = rect.x()
 		y = rect.y()
 		line_height = 0
 		spacing = self.spacing()
 
-		for item in self._item_list:
+		for item in self._itemList:
 			style = item.widget().style()
 			layout_spacing_x = style.layoutSpacing(
 				QSizePolicy.ControlType.PushButton, QSizePolicy.ControlType.PushButton,
@@ -92,6 +100,8 @@ class _layout(QLayout):
 			line_height = max(line_height, item.sizeHint().height())
 
 		return y + line_height - rect.y()
+	#def doLayout
+#class _layout
 
 
 class QFlowTouchWidget(QScrollArea):
@@ -100,9 +110,9 @@ class QFlowTouchWidget(QScrollArea):
 		self.setWidgetResizable(True)
 		self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 		self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-		content = QWidget(self)
-		self.setWidget(content)
-		self.flowLayout=_layout(content)
+		self.content = QWidget(self)
+		self.setWidget(self.content)
+		self.flowLayout=_layout(self.content)
 
 	def addWidget(self, item):
 		self.flowLayout.addWidget(item)
@@ -136,6 +146,10 @@ class QFlowTouchWidget(QScrollArea):
 
 	def minimumSize(self):
 		return(self.flowLayout.minimumSize())
+
+	def clean(self):
+		for idx in reversed(range(0,self.content.layout().count())):
+			self.content.layout().itemAt(idx).widget().setParent(None)
 #class QFlowTouchWidget
 
 if __name__=="__main__":
