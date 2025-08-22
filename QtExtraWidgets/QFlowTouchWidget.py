0a6c6f4b2b239f0c69b3ccdf52862e1b7eb48053
lliurex <juanma1980@gmail.com>
2025-08-22 14:24:36 +0200

    Fast method implemented

--- QtExtraWidgets/QFlowTouchWidget.py
+++ QtExtraWidgets/QFlowTouchWidget.py
@@ -3,24 +3,32 @@
 from PySide6.QtWidgets import QScroller,QScrollerProperties,QWidget,QAbstractItemView,QLayout,QLabel,QSizePolicy,QScrollArea
 from PySide6.QtCore import Qt,QSize,QRect,QPoint,QEvent,Property,Signal
 import PySide6
+import time
 
 class _layout(QLayout):
 	currentItemChanged=Signal("PyObject","PyObject")
-	def __init__(self, parent=None):
+	def __init__(self, parent=None,fastMode=False):
 		super().__init__(parent)
 		self._previousItem=None
 		self._currentItem=None
 		self._currentIndex=-1
 		self._itemList = []
 		self._widgetList = []
+		self._cache={}
+		self.oldRect=QRect(0,0,0,0)
+		self.prevItem=None
 		if parent is not None:
 			self.setContentsMargins(0, 0, 0, 0)
+		self.fastMode=fastMode
 	#def __init__
 
 	def __del__(self):
-		item = self.takeAt(0)
-		while item:
+		try:
 			item = self.takeAt(0)
+			while item:
+				item = self.takeAt(0)
+		except Exception as e:
+			print(e)
 	#def __del__
 
 	def currentItem(self):
@@ -58,7 +66,7 @@ class _layout(QLayout):
 	#def eventFilter
 
 	def count(self):
-		return len(self._itemList)
+		return len(self._itemList)+len(self._cache)
 	#def count
 
 	def currentIndex(self):
@@ -67,25 +75,48 @@ class _layout(QLayout):
 
 	def indexOf(self,item):
 		try:
-			idx=self._itemList.index(item)
-		except:
-			try:
+			if item in self._cache.keys():
+				idx=list(self._cache.keys()).index(item)
+			elif item in self._itemList:
+				idx=self._itemList.index(item)
+			elif item in self._widgetList: 
 				idx=self._widgetList.index(item)
-			except Exception as e:
-				print(e)
+			else:
 				idx=-1
+				for cItem,cWdg in self._cache.items():
+					if cWdg==item:
+						idx=list(self._cache.keys()).index(cItem)
+		except Exception as e:
+			print(e)
 		return(idx)
 	#def indexOf
 
 	def itemAt(self, index):
-		if 0 <= index < len(self._itemList):
-			return self._itemList[index]
+		try:
+			if 0 <= index < len(self._cache):
+				return list(self._cache.keys())[index]
+			else:
+				index=index-len(self._cache)
+				if 0 <= index < len(self._itemList):
+					return self._itemList[index]
+		except Exception as e:
+			print(e)
 		return None
 	#def itemAt
 
 	def takeAt(self, index):
-		if 0 <= index < len(self._itemList):
-			return self._itemList.pop(index)
+		try:
+			if 0 <= index < len(self._cache):
+				item=list(self._cache.keys())[index]
+				self._cache.pop(item)
+				return(item)
+			else:
+				index=index-len(self._cache.keys())
+				if 0 <= index < len(self._itemList):
+					self._widgetList.pop(index)
+					return self._itemList.pop(index)
+		except Exception as e:
+			print(e)
 		return None
 	#def takeAt
 
@@ -119,13 +150,44 @@ class _layout(QLayout):
 		return size
 	#def minimumSize
 
-	def doLayout(self, rect, test_only):
+	def _doLayoutFast(self, rect):
+		x = rect.x()
+		y = rect.y()
+		spacing = self.spacing()
+		itemList=self._itemList
+		try:
+			if len(itemList)>0:
+				item=itemList[-1]
+				#if item.widget()!=self.prevItem:
+				if item.widget()==None:
+					self._itemList.remove(item)
+					self._widgetList.remove(item.widget())
+				else:
+					prevItem=None
+					if len(itemList)>1:
+						prevItem=itemList[-2].widget()
+						while len(self._itemList)>2:
+							self._cache.update({self._itemList.pop(0):self._widgetList.pop(0)})
+					if prevItem!=None:
+						x = prevItem.x()+item.sizeHint().width()
+						y = prevItem.y()
+						x+=spacing
+					if x>rect.right():
+						x=0
+						y = item.sizeHint().height()+y
+						y+=spacing
+					item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+		except Exception as e:
+				print(e)
+		return (y-rect.y())
+	#def _doLayoutFast
+
+	def _doLayout(self, rect, test_only):
 		x = rect.x()
 		y = rect.y()
 		line_height = 0
 		spacing = self.spacing()
 		itemList=self._itemList
-
 		for item in itemList:
 			if item.widget()==None:
 				self._itemList.remove(item)
@@ -155,18 +217,25 @@ class _layout(QLayout):
 			line_height = max(line_height, item.sizeHint().height())
 
 		return y + line_height - rect.y()
+	#def _doLayout
+	
+	def doLayout(self, rect, test):
+		if self.fastMode==True:
+		   return(self._doLayoutFast(rect))
+		else:
+		   return(self._doLayout(rect,test))
 	#def doLayout
 #class _layout
 
 class QFlowTouchWidget(QScrollArea):
-	def __init__(self, parent=None):
+	def __init__(self, parent=None,fastMode=False):
 		super().__init__(parent)
 		self.setWidgetResizable(True)
 		self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 		self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 		self.content = QWidget(self)
 		self.setWidget(self.content)
-		self.flowLayout=_layout(self.content)
+		self.flowLayout=_layout(self.content,fastMode)
 		self.cleaning=False
 
 	#def __init__
